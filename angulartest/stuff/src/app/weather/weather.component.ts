a2f41ca81788959083c53adcd60227594fd0e574
Drumich <Drumich90@gmail.com>
2020-07-21 17:16:31 +0300

    asdada

--- angulartest/stuff/src/app/weather/weather.component.ts
+++ angulartest/stuff/src/app/weather/weather.component.ts
@@ -4,15 +4,20 @@ import { environment } from 'src/environments/environment';
 
 import { map } from 'rxjs/operators';
 
-interface RateDateRates {
-  [k: string]: { [k: string]: number }
-}
+// interface RateDateRates {
+//   [k: string]: { [k: string]: number }
+// }                      // ^ this is for currency stuff
 
-interface RateData {
-  start_at: string;
-  base: string;
-  end_at: string;
-  rates: RateDateRates;
+// interface RateData { // currency stuff
+interface WeatherData {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+  // start_at: string;  // currency stuff
+  // base: string;  // currency stuff
+  // end_at: string;  // currency stuff
+  // rates: RateDateRates; // currency stuff
 }
 
 @Component({
@@ -21,35 +26,52 @@ interface RateData {
   styleUrls: ['./weather.component.scss'],
 })
 export class WeatherComponent implements OnInit {
-  baseRate = 'EUR';
-  symbols = 'USD,GBP';
-  weatherData: any;
 
-  startDate: string;
-  endDate: string;
-  rates: any[];
+  // baseRate = 'EUR';
+  // symbols = 'USD,GBP';
+  weatherData: any;
+  Temperature: [string, any][];
+
+  // startDate: string;
+  // endDate: string;
+  // rates: any[];
+
+
+
+
 
   constructor(private restClient: HttpClient) {}
 
   ngOnInit(): void {
     this.restClient
-      .get<RateData>(this.getRatesUrl())
+      .get<WeatherData>(this.getTemperaturesUrl())
       .subscribe((data) => this.processData(data));
   }
 
-  processData(data: RateData): void {
-    this.startDate = data.start_at;
-    this.endDate = data.end_at;
-    this.rates = Object.entries(data.rates)
-    .map(divainiba=>({
-      date: divainiba[0],
-      rate: divainiba[1]
-    }));
-  }
+  processData(data: WeatherData): void {
+    this.Temperature = Object.entries(data);
+      map(divainiba=>({
+        date: divainiba[0],
+        temperatureC: divainiba[1],
+        temperatureF: divainiba[2],
+        summary: divainiba[3]
+      }));
+    }
+  
 
-  getRatesUrl(): string {
-    return environment.ratesUrl
-      .replace('{base}', this.baseRate)
-      .replace('{symbols}', this.symbols);
+  // processData(data: WeatherData): void {
+  //   this.startDate = data.start_at;
+  //   this.endDate = data.end_at;
+  //   this.rates = Object.entries(data.rates)
+  //   .map(divainiba=>({
+  //     date: divainiba[0],
+  //     rate: divainiba[1]
+  //   }));
+  // }
+
+  getTemperaturesUrl(): string {
+    return environment.temperaturesUrl;
+      // .replace('{base}', this.baseRate)
+      // .replace('{symbols}', this.symbols);
   }
 }
--- angulartest/stuff/src/app/weather/weather.module.ts
+++ angulartest/stuff/src/app/weather/weather.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherComponent } from './weather.component';
-import { WeatherService} from './weather.service';
-import { HttpClientModule} from '@angular/common/http';
+import { WeatherService } from './weather.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [WeatherComponent],
