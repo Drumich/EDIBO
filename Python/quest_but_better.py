2f967414182def53a45bfc3cee8c541e1d0bf6fb
Drumich <58115541+Drumich@users.noreply.github.com>
2020-07-23 15:48:12 +0300

    Create quest_but_better.py

---
+++ Python/quest_but_better.py
@@ -0,0 +1,67 @@
+import time
+import sys
+delay = 2
+
+print("Welcome to our town , what is your name stranger? ")
+name = input(str("Enter your name: "))
+
+
+def intro():
+    print("Nice to meet you %s , our town has been under attack recently by mysterious creature , would you help us?" % name)
+    # time.sleep(delay)
+    # start = input(str("(y/n)"))
+    # if start == "y":
+    #     print("Excellent, let me tell you more about the creature!")
+    #     time.sleep(delay)
+    #     print("The creature has been described as big,black,massive claws and teeth ,red eyed like...creature")
+    #     time.sleep(delay)
+    #     print("Now follow me out of the town, %s" % name)
+    #     time.sleep(delay)
+    # else:
+    #     sys.exit()
+    
+def forest():
+    print("Alright %s , the creature was last seen heading into the forest , i can't go with you , but take this rusty blade.Good Luck !" % name)
+    #time.sleep(delay)
+    
+
+
+
+
+
+
+
+
+intro()
+forest()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
