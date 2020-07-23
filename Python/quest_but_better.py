b85901e2a6eeb2f53c7a5c1ac82ce589bcad5369
Drumich <58115541+Drumich@users.noreply.github.com>
2020-07-23 16:20:01 +0300

    Update quest_but_better.py

--- Python/quest_but_better.py
+++ Python/quest_but_better.py
@@ -1,10 +1,16 @@
 import time
 import sys
+import random
 delay = 2
 
 print("Welcome to our town , what is your name stranger? ")
-name = input(str("Enter your name: "))
 
+# class player:
+#     def __init__(self):
+name = input(str("Enter your name: "))
+        # self.weapon = "Rusty Sword"
+        # self.weapon2 = ""
+        # self.hunger= 0
 
 def intro():
     print("Nice to meet you %s , our town has been under attack recently by mysterious creature , would you help us?" % name)
@@ -23,7 +29,19 @@ def intro():
 def forest():
     print("Alright %s , the creature was last seen heading into the forest , i can't go with you , but take this rusty blade.Good Luck !" % name)
     #time.sleep(delay)
-    
+    print("You start walking into the forest , you hear rumbling.")
+    investigate = input("Do you want to investigate? (y/n)")
+    if investigate == "y":
+        print("You walk closer to the rumbling sounds.")
+        #time.sleep(delay)
+        action = input("You see a rabbit , would you try and catch it? (y/n)")
+        if action == "y":
+            print("You try to catch the rabbit.")
+            for num in random.randint(1,11):
+                if num >= 5:
+                    print("You caught the rabbit.")
+                else:
+                    print("You fail to catch the rabbit , it was too quick.")
 
 
 
