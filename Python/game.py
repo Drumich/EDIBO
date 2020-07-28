e8b2307ef97e5fa7dcbc708682ff89b41d6b61e0
Drumich <58115541+Drumich@users.noreply.github.com>
2020-07-28 18:12:36 +0300

    added bg

--- Python/game.py
+++ Python/game.py
@@ -3,15 +3,17 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((1280,720))
 
 pygame.display.set_caption("Hello")
 
 x = 50
-y = 450
+y = 550
 height = 50
 width = 50
 vel = 5
+
+bg = pygame.image.load("a.jpg")
 
 screen = 500
 
@@ -54,8 +56,8 @@ while run:
             isJump = False
             jumpCount = 10
 
-    win.fill((0,0,0))
-    pygame.draw.rect(win , (0,255,255) , (x, y, width, height))
+    win.blit(bg, (0,0))
+    pygame.draw.rect(win , (0,0,0) , (x, y, width, height))
     pygame.display.update()
 
 
